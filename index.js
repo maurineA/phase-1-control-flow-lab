50a05d1a0db5c4796491bb16b1461a9760f21cd1
Mwambui123 <maurinawambuia@gmail.com>
2023-10-05 12:04:20 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+  
+if (number <= 400) {
+  return "This one is on me!";
+
+} else if (number > 2000 &&  number <= 2500) {
+  return "I will gladly take your thirty bucks.";
+
+} else {
+  return "No can do.";
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC"){
+  return "Ok, sounds good.";
+} else {
+  return "No go.";
+}
+}
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  if(tip === "generous"){
+    return "Thank you so much.";
+  } else if(tip === "not as generous"){
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
